9fa13facbacef7ecad796c48e6937c8f59775d64
Sujal Ambatkar <sujalambatkar@Sujals-MacBook-Air.local>
2026-03-20 13:44:59 +0530

    fix cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Student Portal API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://student-portal-theta-mauve.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
